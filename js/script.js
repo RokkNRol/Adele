cb3e9b56bed29e8347da12e99370fe6c3657beb1
RockNRoll <himera80@mail.ru>
2019-03-30 15:21:13 +0200

    menu

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const menuFour = document.querySelector('.menuFour');
+
+function addClassFunFour() {
+   this.classList.toggle("clickMenuFour");
+}
+menuFour.addEventListener('click', addClassFunFour);
