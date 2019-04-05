451ed29b5aa2baf4774e2096b75ad358678cdc87
RockNRoll <himera80@mail.ru>
2019-04-05 20:47:34 +0300

    /slider

--- js/script.js
+++ js/script.js
@@ -5,3 +5,36 @@ function addClassFunFour() {
    this.classList.toggle("clickMenuFour");
 }
 menuFour.addEventListener('click', addClassFunFour);
+$(function () {
+
+   $('.top-slider').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+   });
+
+   $('.menu--btn').click(function () {
+      $('.header--menu-list').slideToggle();
+   });
+
+   $(".top--to-go").on("click", "a", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+
+      //забираем идентификатор бока с атрибута href
+      var id = $(this).attr('href'),
+
+         //узнаем высоту от начала страницы до блока на который ссылается якорь
+         top = $(id).offset().top;
+
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({
+         scrollTop: top
+      }, 900);
+   });
+
+
+});
